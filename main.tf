1a5a2837adea4ca088492460ef8bd7c0e7b3e621
zimisele <43851011+zimisele@users.noreply.github.com>
2025-03-24 18:55:44 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -143,6 +143,15 @@ resource "aws_instance" "ec2_instance" {
   instance_type = "t3.micro"
   subnet_id     = aws_subnet.public.id
   vpc_security_group_ids  = [aws_security_group.ec2_sg.id]
+  user_data = <<-EOF
+            #!/bin/bash
+            sudo apt update
+            sudo apt install docker.io -y
+            sudo apt-get install zip unzip -y
+            curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+            unzip awscliv2.zip  # Extract the AWS CLI installer
+             sudo ./aws/install   
+            EOF
 
   tags = {
 	Name = "EC2 Instance"
