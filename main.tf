6055916490c9c54d561cb3d26bfc274503b9faf4
zimisele <43851011+zimisele@users.noreply.github.com>
2025-03-24 14:16:34 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -98,6 +98,12 @@ resource "aws_security_group" "ec2_sg" {
 	protocol    = "tcp"
 	cidr_blocks = ["10.0.0.0/16"]
   }
+   ingress {
+        from_port   = 22
+        to_port     = 22
+        protocol    = "tcp"
+        cidr_blocks = ["0.0.0.0/0"] 
+  }
 }
 # this security group is for rds
 resource "aws_security_group" "rds_sg" {
@@ -115,6 +121,19 @@ resource "aws_security_group" "rds_sg" {
 	to_port     = 3306
 	protocol    = "tcp"
 	security_groups = [aws_security_group.ec2_sg.id]
+  }
+   ingress {
+       from_port   = 80
+       to_port     = 80
+       protocol    = "tcp"
+       cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  ingress {
+     from_port   = 443
+     to_port     = 443
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]  # Allow HTTPS from anywhere
   }
 }
 
